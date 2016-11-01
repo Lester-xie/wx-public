54e41dbacd19eb524a6b0b5ab748833160953ef3
nali <bulger.xie@ximalaya.com>
2016-11-01 18:36:31 +0800

    update

--- controller/getMessage.js
+++ controller/getMessage.js
@@ -14,7 +14,7 @@ app.use('/wechat', wechat(config.wechat_middle, function (req, res, next) {
     if(message.Content=="二狗"){
         res.reply("敢这么叫哥的,一定是烂葵花,也可能是安大官人");
     }if(message.Content=="你真帅"){
-        res.reply("真给面子,muamua,<a href='www.baidu.com'>百度</a>");
+        res.reply("<a href='www.baidu.com'>百度</a>");
     }else{
         res.reply([
             "",
