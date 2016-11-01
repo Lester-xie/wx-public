6c9939ebe21ff5dd8765f84915bc01e659278598
nali <bulger.xie@ximalaya.com>
2016-11-01 14:43:45 +0800

    图文消息回复

--- server/config.js
+++ server/config.js
@@ -7,6 +7,11 @@ const config = {
         token:"imbulger",
         appid:"wx871a92db8d222adc",
         secret:"ee71ecf6e07994ffa43694b0bd553bab"
+    },
+    wechat_middle:{
+        token: 'imbulger',
+        appid: 'wx871a92db8d222adc',
+        encodingAESKey: 'hS8a15rDJfM9Kpr2yl4uJXeLpSvz035fi7P8FRP0m8o'
     }
 }
 
