53f9993488768c65b727ef090f0b28e71143a48c
nali <bulger.xie@ximalaya.com>
2016-11-02 22:37:45 +0800

    消息文本修改

--- controller/getMessage.js
+++ controller/getMessage.js
@@ -23,8 +23,8 @@ app.use('/wechat', wechat(config.wechat, function (req, res, next) {
     //关注后发消息
     if (message.Event == "subscribe") {
         res.reply({
-            content: '哈,终于等到你。为了能及时接收行业最新最前沿技术资讯,帮助个人成长,所以诞生了这个消息驿站。\n' +
-            '回复博客对应的数字,即可获取该博客的最新文章链接\n\n' + name + '回复0查看博客列表',
+            content: '  哈,终于等到你。为了能及时接收行业最新最前沿技术资讯,帮助个人成长,所以诞生了这个消息驿站。\n' +
+            '  回复博客对应的数字,即可获取该博客的最新文章链接。\n\n' + name + '\n回复0查看博客列表',
             type: 'text'
         });
     }
@@ -39,13 +39,13 @@ app.use('/wechat', wechat(config.wechat, function (req, res, next) {
             resMsg = "别瞎搞了,塘子里还没这条神龙(1-6),你召唤不出来的";
             res.reply({content: resMsg, type: "text"});
         }else if(number==0){
-            res.reply({content: "使劲记下来啊,魂淡\n"+name, type: "text"});
+            res.reply({content: "加油使劲记下来啊,魂淡\n"+name, type: "text"});
         } else {
             Model.find({code: number}, function (err, data) {
                 if (data.length == 0) {
                     resMsg = "别催别催,在来的路上了";
                 } else {
-                    resMsg = urlName[number] + "\n·" + data[0].title + "\n·" + data[0].url;
+                    resMsg = number+"."+urlName[number] + "\n·" + data[0].title + "\n·" + data[0].url;
                 }
                 console.log(resMsg);
                 res.reply({content: resMsg, type: "text"});
