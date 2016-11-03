63748cc543e6296f98ecf04da59af96dd2f42a2e
nali <bulger.xie@ximalaya.com>
2016-11-03 20:13:30 +0800

    菜单创建

--- app.js
+++ app.js
@@ -6,4 +6,5 @@ const app = require("./server/server");
 // require("./controller/serverCheck");
 require("./controller/getMessage");
 require("./controller/crawler");
+require("./controller/createMenu")();
 app.listen("80");
