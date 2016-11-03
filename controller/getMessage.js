b1e4cb686a2e5187bcade2985a102d15cb5f0e46
nali <bulger.xie@ximalaya.com>
2016-11-03 21:31:32 +0800

    update

--- controller/getMessage.js
+++ controller/getMessage.js
@@ -4,7 +4,6 @@
 
 const app = require("../server/server");
 const config = require('../server/config');
-const config1 = require('../server/config1');
 const Model = require("../models/crawlInfo");
 const wechat = require('wechat');
 
@@ -15,9 +14,6 @@ const wxConfig = {
 };
 
 console.log(wxConfig);
-
-console.log(config1.wechat);
-
 
 const urlName = ["淘宝前端团队", "凹凸实验室", "百度前端研发部", "奇舞团", "京东设计中心", "阮一峰blog"];
 const name = '1：<a href="http://taobaofed.org">' + urlName[0] + '</a>\n' +
